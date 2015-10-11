47dff80e38720fbc4f05df4315bac0a056ea65e1
zllc <zllc163@163.com>
2015-10-11 19:36:43 +0800

    change

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,6 @@
 ---
     layout: null
----
-
-/**
- * 页面ready方法
- */
-$(document).ready(function() {
-
-    console.log("你不乖哦，彼此之间留点神秘感不好吗？");
-
-    backToTop();
-    search();
-});
-
+--- 
 /**
  * 回到顶部
  */
@@ -43,15 +31,4 @@ function backToTop() {
 }
 
 function search(){
-    (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-        (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-        e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.appendChild(s);
-    })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
-
-    _st('install','{{site.swiftype_searchId}}','2.0.0');
 }
-
-
-
-
-
